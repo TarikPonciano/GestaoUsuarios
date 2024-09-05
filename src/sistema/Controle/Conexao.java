2566109f5d515bf9c1c07a85c706474b7f511603
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2024-09-05 19:50:58 -0300

    Criação do Conexão.Java

---
+++ src/sistema/Controle/Conexao.java
@@ -0,0 +1,72 @@
+package sistema.Controle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Conexao {
+	// Informações para conectar com o banco de dados
+	private String url;
+	private String user;
+	private String password;
+	private String database;
+	
+	// Objetos usados para consultar e manipular o banco
+	private Connection con = null; // Conexão que será estabelecida
+	private Statement st = null; // Objeto que permite executar comandos SQL
+	private PreparedStatement pst = null; // Objeto que permite executar comandos SQL que usem variáveis do java
+	private ResultSet rs = null; // Objeto que guarda os resultados das consultas (SELECT)
+	
+	public Conexao ( String user, String password, String database) {
+		this.url = "jdbc:mysql://localhost:3306/";
+		this.user = user;
+		this.password = password;
+		this.database = database;
+	}
+	
+	public void conectar() {
+		try {
+			if (con == null || con.isClosed()) {
+				con = DriverManager.getConnection(url+database, user, password);
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	public void desconectar() {
+		
+		try {
+			if (rs != null && !rs.isClosed()) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		try {
+			if (pst != null && !pst.isClosed()) {
+				pst.close();
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		try {
+			if (st != null && !st.isClosed()) {
+				st.close();
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		try {
+			if (con != null && !con.isClosed()) {
+				con.close();
+			}
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	
+	}
+	}
